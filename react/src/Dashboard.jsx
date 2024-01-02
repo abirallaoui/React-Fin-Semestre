b2716f71fab45ef531353cc7f3e14ca957971395
abirallaoui <allaouiabir2@gmail.com>
2024-01-02 12:11:55 +0100

    init

---
+++ react/src/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Dashboard() {
+
+  return (
+    <div >
+                <img style={{ width: '600px', height: '400px',marginLeft:'200px' }} src="/FastContact-removebg-preview.png" alt="IMAGE"    />
+
+
+    </div>
+  )
+}
+
+export default Dashboard
